2dd2d95e826ef038a0cff837814d95a7662b7fa5
ratna1992 <ratnasrinivas1992@gmail.com>
2022-12-15 10:50:00 +0530

    first commit

---
+++ src/main/java/com/boot/mysql/k8s/BootMysqlK8sApplication.java
@@ -0,0 +1,13 @@
+package com.boot.mysql.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMysqlK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMysqlK8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/mysql/k8s/BootMysqlK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.mysql.k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMysqlK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
